0829a990256a3c88e4a00a546d2de0ebf78010fd
ultron <103036831+vj2303@users.noreply.github.com>
2024-12-16 14:57:12 +0530

    Add files via upload

---
+++ src/app/delete-account/page.tsx
@@ -0,0 +1,17 @@
+import NavBar from '@/components/common/NavBar'
+import FooterSection from '@/components/sections/FooterSection'
+import React from 'react'
+import Form from './Form'
+
+const page = () => {
+  return (
+    <div>
+        <NavBar/>
+        <Form/>
+        <FooterSection/>
+
+    </div>
+  )
+}
+
+export default page
